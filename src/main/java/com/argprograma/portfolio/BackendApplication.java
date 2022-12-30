ec3ce3b2f3d5acdeb528bdc0401f6d56e7e3219a
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-12-30 13:59:05 -0300

    portfolio init

---
+++ src/main/java/com/argprograma/portfolio/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.argprograma.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
